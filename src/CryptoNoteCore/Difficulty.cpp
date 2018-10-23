4a5f2a5b459f5cd993f767bcf06697231dc65509
Amity <42378180+Amity-Core@users.noreply.github.com>
2018-10-23 15:35:12 +0100

    Update Difficulty.cpp
    Cheers @TurtleCoin for the guide on properly implementing this, :pepe: moments

--- src/CryptoNoteCore/Difficulty.cpp
+++ src/CryptoNoteCore/Difficulty.cpp
@@ -9,14 +9,61 @@
 
 #include <config/CryptoNoteConfig.h>
 
+// LWMA-3 difficulty algorithm
+// Copyright (c) 2017-2018 Zawy, MIT License
+// https://github.com/zawy12/difficulty-algorithms/issues/3
+uint64_t nextDifficultyV6(std::vector<uint64_t> timestamps, std::vector<uint64_t> cumulativeDifficulties)
+{
+    uint64_t T = CryptoNote::parameters::DIFFICULTY_TARGET;
+    uint64_t N = CryptoNote::parameters::DIFFICULTY_WINDOW_V3;
+    uint64_t L(0), ST, sum_3_ST(0), next_D, prev_D, nextTime, lastTime;
+
+	
+    /* If we are starting up, returning a difficulty guess. If you are a
+       new coin, you might want to set this to a decent estimate of your
+       hashrate */
+    if (timestamps.size() < static_cast<uint64_t>(N+1))
+    {
+        return 5000;
+    }
+	
+	lastTime = timestamps[0];
+	
+    for ( int64_t i = 1; i <= N; i++) {
+      if (static_cast<int64_t>(timestamps[i]) > lastTime) {
+      nextTime = timestamps[i];
+      } else { nextTime = lastTime+1; }
+      ST = std::min(6*T,nextTime - lastTime);
+      lastTime = nextTime;
+      L +=  ST * i;
+      if ( i > N-3 ) { sum_3_ST += ST; }
+    }
+
+    next_D = (static_cast<int64_t>(cumulativeDifficulties[N] - cumulativeDifficulties[0]) * T * (N+1) * 99) / (100 * 2 * L);
+
+    prev_D = cumulativeDifficulties[N] - cumulativeDifficulties[N-1];
+
+    next_D = std::max((prev_D * 67) / 100, std::min(next_D, (prev_D * 150) / 100));
+
+    if (sum_3_ST < (9 * T) / 10)
+    {  
+        next_D = std::max(next_D, (prev_D * 108) / 100);
+    }
+
+    return static_cast<uint64_t>(next_D);
+}
+
 // LWMA-2 difficulty algorithm 
 // Copyright (c) 2017-2018 Zawy, MIT License
 // https://github.com/zawy12/difficulty-algorithms/issues/3
 uint64_t nextDifficultyV5(std::vector<uint64_t> timestamps, std::vector<uint64_t> cumulativeDifficulties)
 {
-    uint64_t T = CryptoNote::parameters::DIFFICULTY_TARGET;
-    uint64_t N = CryptoNote::parameters::DIFFICULTY_WINDOW_V3;
-    uint64_t L(0), ST, sum_3_ST(0), next_D, prev_D, lastTime, nextTime;
+    int64_t T = CryptoNote::parameters::DIFFICULTY_TARGET;
+    int64_t N = CryptoNote::parameters::DIFFICULTY_WINDOW_V3;
+    int64_t L(0), ST, sum_3_ST(0), next_D, prev_D;
+	int64_t nextTime, lastTime;
+
+	lastTime = timestamps[0];
 
     /* If we are starting up, returning a difficulty guess. If you are a
        new coin, you might want to set this to a decent estimate of your
@@ -26,8 +73,6 @@ uint64_t nextDifficultyV5(std::vector<uint64_t> timestamps, std::vector<uint64_t
         return 5000;
     }
 
-    lastTime = timestamps[0];
-    
     for ( int64_t i = 1; i <= N; i++) {  
       if (static_cast<int64_t>(timestamps[i]) > lastTime  ) {   
       nextTime = timestamps[i];
@@ -36,9 +81,9 @@ uint64_t nextDifficultyV5(std::vector<uint64_t> timestamps, std::vector<uint64_t
       ST = std::min(6*T,nextTime - lastTime);
       lastTime = nextTime;
 
-      L +=  ST * i;
+      L +=  ST * i ;
 
-      if ( i > N-3 ) { sum_3_ST += ST; }
+      if ( i > N-3 ) { sum_3_ST += ST; } 
     }
 
     next_D = (static_cast<int64_t>(cumulativeDifficulties[N] - cumulativeDifficulties[0]) * T * (N+1) * 99) / (100 * 2 * L);
