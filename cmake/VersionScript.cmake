10f2b0a4bd976ece0ba6b3140e0e293d3a3cd1c0
Michael Herwig <michael.herwig@hotmail.de>
2018-11-16 17:07:09 +0100

    [CMAKE] include dependency refactoring

--- cmake/VersionScript.cmake
+++ cmake/VersionScript.cmake
@@ -18,7 +18,7 @@ else()
         # Get the current working branch
         execute_process(
             COMMAND git rev-parse --abbrev-ref HEAD
-            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+            WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
             OUTPUT_VARIABLE GIT_BRANCH
             OUTPUT_STRIP_TRAILING_WHITESPACE
         )
@@ -26,7 +26,7 @@ else()
         # Get the latest abbreviated commit hash of the working branch
         execute_process(
             COMMAND git log -1 --format=%h
-            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+            WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
             OUTPUT_VARIABLE GIT_COMMIT_HASH
             OUTPUT_STRIP_TRAILING_WHITESPACE
         )
