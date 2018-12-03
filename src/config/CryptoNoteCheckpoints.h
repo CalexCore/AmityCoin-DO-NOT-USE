05e5a63b49aff3aa298cff337fbbbf188029bb76
hooftly <mhooft@tf-dev.org>
2018-12-03 11:51:44 -0700

    Revert "[CORE] Checkpoints"
    This reverts commit c1049b16ed944492a726958baa875fbafead84d0.

--- src/config/CryptoNoteCheckpoints.h
+++ src/config/CryptoNoteCheckpoints.h
@@ -30,9 +30,6 @@ struct CheckpointData {
 const std::initializer_list<CheckpointData> CHECKPOINTS = {
   {       0, "34f61e2d54906689fc249f0d1aecda614ad3434074009a9bfa350bc6b9ef1ff5"},
   {       2, "7ad1ec5d6aa83e315cff3a7eaf09cd75973bf6526036e55373dd01c484f5ac99"},
-  {    5000, "09158af9e50f58778e87e2c47450be18a9fdaf8c853d06523da8824e04ab3f51"},
-  {   10000, "f393e6cebd0cd2f306920b26a6b2c4ae3fadb28b2399b9af93218bb3e859154a"},
-  {   15000, "43c306f93ba75dc7115eb484f840a0da231be83bf61a5ff464d4c0144a33dce4"},
-  {   20000, "429fe402effe8e53f72a1d054fe17162abb56fda9127d99ac33a6a2c1e036d3c"}
+  { 5000, "09158af9e50f58778e87e2c47450be18a9fdaf8c853d06523da8824e04ab3f51"}
 };
 }
